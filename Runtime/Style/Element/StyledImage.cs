7f78f6afd34d880110cba67c5307dd6c73e6811e
Maxim.Bagashev <ThreeDISevenZeroR@gmail.com>
2020-10-04 16:25:05 +0400

    Styled text and image implementation

---
+++ Runtime/Style/Element/StyledImage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace ThreeDISevenZeroR.Stylist
+{
+    public class StyledImage : StyledElement<ImageStyle, ImageStyleData>
+    {
+        public Image image;
+        
+        protected override void ApplyStyle(ImageStyleData style)
+        {
+            StyleUtils.Apply(style, image);
+        }
+    }
+}
---
+++ Runtime/Style/Element/StyledText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace ThreeDISevenZeroR.Stylist
+{
+    public class StyledText : StyledElement<TextStyle, TextStyleData>
+    {
+        public Text text;
+        
+        protected override void ApplyStyle(TextStyleData style)
+        {
+            StyleUtils.Apply(style, text);
+        }
+    }
+}
